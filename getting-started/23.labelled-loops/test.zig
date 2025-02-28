f069e505ecbb560e47c601c762ad5d0da821bef5
t-ash0410 <t_ash0410@yahoo.co.jp>
2025-02-28 20:06:10 +0900

    labelled loops

---
+++ getting-started/23.labelled-loops/test.zig
@@ -0,0 +1,23 @@
+const expect = @import("std").testing.expect;
+
+test "nested continue" {
+    var count: usize = 0;
+    outer: for ([_]i32{ 1, 2, 3, 4, 5, 6, 7, 8 }) |_| {
+        for ([_]i32{ 1, 2, 3, 4, 5 }) |_| {
+            count += 1;
+            continue :outer;
+        }
+    }
+    try expect(count == 8);
+}
+
+test "normal continue" {
+    var count: usize = 0;
+    for ([_]i32{ 1, 2, 3, 4, 5, 6, 7, 8 }) |_| {
+        for ([_]i32{ 1, 2, 3, 4, 5 }) |_| {
+            count += 1;
+            continue;
+        }
+    }
+    try expect(count == 40);
+}
